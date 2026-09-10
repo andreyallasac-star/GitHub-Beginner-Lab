bfdb24de1c0d01e7afa4621d28817689f0b6ce1f
Neil Andrei Casalla <andreicasalla@gmail.com>
2026-09-10 22:18:45 +0800

    Changed number

--- StudentProfile/Form1.Designer.cs
+++ StudentProfile/Form1.Designer.cs
@@ -50,7 +50,7 @@
             this.lblContact.Name = "lblContact";
             this.lblContact.Size = new System.Drawing.Size(232, 20);
             this.lblContact.TabIndex = 1;
-            this.lblContact.Text = "Contact Number: 09171234567";
+            this.lblContact.Text = "Contact Number: 09152191699";
             // 
             // Form1
             // 
