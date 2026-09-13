04a85d925613bcef04d7eac13d60d49415e9b9e5
Neil Andrei Casalla <andreicasalla@gmail.com>
2026-09-13 19:06:01 +0800

    recommit #2

--- StudentProfile/Form1.Designer.cs
+++ StudentProfile/Form1.Designer.cs
@@ -74,9 +74,9 @@
             this.lblYearLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.lblYearLevel.Location = new System.Drawing.Point(57, 126);
             this.lblYearLevel.Name = "lblYearLevel";
-            this.lblYearLevel.Size = new System.Drawing.Size(157, 20);
+            this.lblYearLevel.Size = new System.Drawing.Size(221, 20);
             this.lblYearLevel.TabIndex = 3;
-            this.lblYearLevel.Text = "Year Level: 2nd Year";
+            this.lblYearLevel.Text = "Year Level: 1st Year Alternate";
             // 
             // lblStudentId
             // 
